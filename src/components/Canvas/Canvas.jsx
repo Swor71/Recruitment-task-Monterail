8577bfb6c70f9a482467f57a93562b90b52e4d0a
Marcin Swornowski <swor71@gmail.com>
2018-09-14 19:22:33 +0200

    Add layout for coordinator component

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -5,6 +5,7 @@ const Canvas = props => {
   return (
     <div className="canvas">
       <h3 className="canvas__title">{props.title}</h3>
+      <div className="canvas__content">{props.children}</div>
     </div>
   );
 };
